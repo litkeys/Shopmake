953443f54419f42689b0c54e839dd599c1139fac
Ninefigurelegend <ninefigurelegend@gmail.com>
2025-08-09 21:14:41 +1000

    FIx
    Build should work

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
 const nextConfig = {
 	output: "standalone",
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			config.output.path = path.join(process.cwd(), ".next/server");
-		}
-		return config;
-	},
 };
 
 module.exports = nextConfig;
