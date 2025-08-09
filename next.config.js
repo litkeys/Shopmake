b92d06334f96aa22981044f8fd96e73f04bc34ec
Ninefigurelegend <ninefigurelegend@gmail.com>
2025-08-09 20:48:10 +1000

    Fix
    Now includes section preset folder in build to allow serverless functions to access it

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require("path");
+
+const nextConfig = {
+	output: "standalone",
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.output.path = path.join(process.cwd(), ".next/server");
+		}
+		return config;
+	},
+};
 
 module.exports = nextConfig;
